db9532f9a861101dadd7e5f13b9ab288cdc210ba
anastasiia-peleshok <anastasiia.peleshok@gmail.com>
2024-04-25 15:55:35 +0300

    Removed extra class

--- src/main/java/com/anastasiia/assignment/AssignmentApplication.java
+++ src/main/java/com/anastasiia/assignment/AssignmentApplication.java
@@ -1,14 +1,10 @@
 package com.anastasiia.assignment;
 
-import com.anastasiia.assignment.config.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ApplicationProperties.class)
 @EnableJpaRepositories
 public class AssignmentApplication {
 
